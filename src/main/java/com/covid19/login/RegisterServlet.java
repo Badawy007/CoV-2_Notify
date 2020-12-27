fc40f4efbe8f09705e87123247288b172183a28a
BadawyJr <a.badawy007@gmail.com>
2020-12-27 23:43:18 +0100

    Signup : Create account and connect to DB

---
+++ src/main/java/com/covid19/login/RegisterServlet.java
@@ -0,0 +1,46 @@
+package com.covid19.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    private DatabaseSetup dbSetup;
+    private User user;
+
+    public RegisterServlet() {
+        user  = new User();
+        dbSetup = new DatabaseSetup();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        user.setUsername(request.getParameter("username"));
+        String pass1 = request.getParameter("password");
+        String pass2 = request.getParameter("confirm");
+        if (pass1.equals(pass2)){
+            user.setPassword(pass1);
+            out.println(user.getUsername());
+            out.println(user.getPassword());
+        }else{
+            out.println("Passwords don't match");
+        }
+
+        try {
+            dbSetup.registerUser(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("signupRedirect.jsp");
+
+
+    }
+
+
+}
---
+++ src/main/webapp/signupRedirect.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: BJR-Desktop
+  Date: 12/27/2020
+  Time: 10:50 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Redirect</title>
+</head>
+<body>
+<h1> Sign up successful </h1>
+</body>
+</html>
